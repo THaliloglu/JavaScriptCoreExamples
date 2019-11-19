c1f5a5f9287fada9416b46334086d94c4e518fdd
Tolga Haliloglu <tolga.haliloglu.28@gmail.com>
2019-11-19 22:52:10 +0300

    Initial Commit - Working version

---
+++ JavaScriptCoreExamples/Supported Files/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JavaScriptCoreExamples
+//
+//  Created by Tolga Haliloğlu on 20.07.2019.
+//  Copyright © 2019 TH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JavaScriptCoreExamples/Supported Files/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  JavaScriptCoreExamples
+//
+//  Created by Tolga Haliloğlu on 20.07.2019.
+//  Copyright © 2019 TH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
